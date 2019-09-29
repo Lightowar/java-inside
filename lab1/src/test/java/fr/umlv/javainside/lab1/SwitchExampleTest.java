3e50c47bac960723ed750dc55c6faa125d7e1e39
nicol <nicol@DESKTOP-RTTCFF5>
2019-09-29 22:27:23 +0200

    Exercice 2-3

---
+++ lab1/src/test/java/fr/umlv/javainside/lab1/SwitchExampleTest.java
@@ -0,0 +1,36 @@
+package fr.umlv.javainside.lab1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SwitchExampleTest {
+    @Test
+    void dog() {
+        assertEquals(1, SwitchExample.switchExample("dog"));
+    }
+    
+    @Test
+    void cat() {
+        assertEquals(2, SwitchExample.switchExample("cat"));
+    }
+    
+    @Test
+    void other() {
+        assertEquals(4, SwitchExample.switchExample("frog"));
+    }
+    @Test
+    void dog2() {
+        assertEquals(1, SwitchExample.switchExample2("dog"));
+    }
+    
+    @Test
+    void cat2() {
+        assertEquals(2, SwitchExample.switchExample2("cat"));
+    }
+    
+    @Test
+    void other2() {
+        assertEquals(4, SwitchExample.switchExample2("frog"));
+    }
+}
