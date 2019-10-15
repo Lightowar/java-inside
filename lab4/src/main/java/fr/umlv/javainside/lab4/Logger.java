9d6f38830a57776c53144cd035585da3ed258988
nicolas <nicolas.baticle@gmail.com>
2019-10-15 11:19:30 +0200

    Logger

--- lab4/src/main/java/fr/umlv/javainside/lab4/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/lab4/Logger.java
@@ -1,17 +1,26 @@
 package fr.umlv.javainside.lab4;
 
+import static java.lang.invoke.MethodHandles.publicLookup;
+import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodType.methodType;
+
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public interface Logger {
 	public void log(String message);
 
 	public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) {
+		Objects.requireNonNull(declaringClass);
+		Objects.requireNonNull(consumer);
 		var mh = createLoggingMethodHandle(declaringClass, consumer);
 		return new Logger() {
 			@Override
 			public void log(String message) {
+				Objects.requireNonNull(message);
 				try {
 					mh.invokeExact(message);
 				} catch (Throwable t) {
@@ -28,7 +37,12 @@ public interface Logger {
 	}
 
 	private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
-		// TODO
-		return null;
+		MethodHandle mh;
+		try {
+			mh = publicLookup().findVirtual(Consumer.class, "accept", methodType(void.class, Object.class));
+		} catch (NoSuchMethodException | IllegalAccessException e) {
+			throw new AssertionError(e);
+		}
+		return insertArguments(mh, 0, consumer).asType(methodType(void.class, String.class));
 	}
 }
