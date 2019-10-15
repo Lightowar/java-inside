881a7c45f90326b5c474a5499071dec4425f1422
nicolas <nicolas.baticle@gmail.com>
2019-10-15 09:44:24 +0200

    Suite initialisation

--- lab4/src/main/java/fr/umlv/javainside/lab4/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/lab4/Logger.java
@@ -1,5 +1,34 @@
 package fr.umlv.javainside.lab4;
 
-public class Logger {
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.function.Consumer;
 
+public interface Logger {
+	public void log(String message);
+
+	public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) {
+		var mh = createLoggingMethodHandle(declaringClass, consumer);
+		return new Logger() {
+			@Override
+			public void log(String message) {
+				try {
+					mh.invokeExact(message);
+				} catch (Throwable t) {
+					if (t instanceof RuntimeException) {
+						throw (RuntimeException) t;
+					}
+					if (t instanceof Error) {
+						throw (Error) t;
+					}
+					throw new UndeclaredThrowableException(t);
+				}
+			}
+		};
+	}
+
+	private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
+		// TODO
+		return null;
+	}
 }
--- lab4/src/test/java/fr/umlv/javainside/lab4/LoggerTest.java
+++ lab4/src/test/java/fr/umlv/javainside/lab4/LoggerTest.java
@@ -1,5 +1,13 @@
 package fr.umlv.javainside.lab4;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class LoggerTest {
 
+	@Test
+	void test() {
+		assertTrue(true);
+	}
 }
