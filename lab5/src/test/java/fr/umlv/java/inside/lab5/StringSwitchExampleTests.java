0490dc4ec4c803c5304b2cf7cbef8c7bbf8cdb44
nicolas <nicolas.baticle@gmail.com>
2019-10-22 09:50:34 +0200

    refactoring des tests et tests de stringSwitch2

--- lab5/src/test/java/fr/umlv/java/inside/lab5/StringSwitchExampleTests.java
+++ lab5/src/test/java/fr/umlv/java/inside/lab5/StringSwitchExampleTests.java
@@ -3,19 +3,28 @@ package fr.umlv.java.inside.lab5;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static fr.umlv.java.inside.lab5.StringSwitchExample.*;
 
 public class StringSwitchExampleTests {
 
-	@Test
-	public void testStringSwitch() {
-		assertAll(
-				()->assertEquals(0, stringSwitch("foo")),
-				()->assertEquals(1, stringSwitch("bar")),
-				()->assertEquals(2, stringSwitch("bazz")),
-				()->assertEquals(-1, stringSwitch("fooz"))
-		);
+	@ParameterizedTest
+	@MethodSource("allFunct")
+	public void testStringSwitch(ToIntFunction<String> arg) {
+		assertAll(() -> assertEquals(0, arg.applyAsInt("foo")), () -> assertEquals(1, arg.applyAsInt("bar")),
+				() -> assertEquals(2, arg.applyAsInt("bazz")), () -> assertEquals(-1, arg.applyAsInt("fooz")));
+	}
+
+	private static Stream<ToIntFunction<String>> allFunct() {
+		List<ToIntFunction<String>> lst = Arrays.asList(s -> stringSwitch(s), s -> stringSwitch2(s));
+		return lst.stream();
 	}
 }
